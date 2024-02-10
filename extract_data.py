dc2ac316ed5b8e838f23ca0ed490cda0b3a68fa8
Sammy Taubman <staubman1@gmail.com>
2024-02-10 11:28:33 -0500

    Initial commit

---
+++ extract_data.py
@@ -0,0 +1,6 @@
+from sportsipy.nfl.teams import Teams
+import pandas as pd
+
+# note: we had to fix sportsipy
+# created issue detailing solution (https://github.com/roclark/sportsipy/issues/791)
+teams = Teams()
