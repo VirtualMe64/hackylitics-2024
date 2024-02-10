005bd4e75ba90e53ade93dce1157b6cc69f42931
Hannah Tourial <45826711+hannahtourial@users.noreply.github.com>
2024-02-10 11:55:01 -0500

    Update sports_gui.py

--- sports_gui.py
+++ sports_gui.py
@@ -1,18 +1,40 @@
 import sys
 import pandas as pd
-from PyQT5.QTWidgets import (
+from PyQt5.QtWidgets import (
     QApplication,
     QWidget,
     QPushButton,
     QHBoxLayout,
     QVBoxLayout,
-    QLabel
+    QLabel,
+    QSlider
 )
+from PyQt5.QtCore import *
 
 import datetime
 
 class Sports(QWidget):
     def __init__(self):
-        super().__init__
+        super().__init__()
 
         self.setWindowTitle('Who Will Win the Super Bowl?')
+        self.overall=QHBoxLayout()
+        self.stats=QVBoxLayout()
+        self.predictions=QVBoxLayout()
+
+        self.stat1=QLabel('Stat 1')
+        self.stats.addWidget(self.stat1)
+        self.stat1s=QSlider(Qt.Horizontal)
+        self.stats.addWidget(self.stat1s)
+
+        self.overall.addLayout(self.stats)
+        self.setLayout(self.overall)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = Sports()
+    main.show()
+    exit_code = app.exec_()
+    sys.exit(exit_code)
